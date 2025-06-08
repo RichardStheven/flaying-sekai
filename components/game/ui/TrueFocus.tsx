093613559c1ff18ad675d62b21f6bda5d3b7e961
Richard <richard@MacBook-Air-de-Richard.local>
2025-06-08 08:42:13 -0300

    est 1

--- components/game/ui/TrueFocus.tsx
+++ components/game/ui/TrueFocus.tsx
@@ -82,8 +82,10 @@ const TrueFocus: React.FC<TrueFocusProps> = ({
                 return (
                     <span
                         key={index}
-                        ref={(el) => (wordRefs.current[index] = el)}
-                        className="relative text-[3rem] font-black cursor-pointer"
+                        ref={(el) => {
+                            wordRefs.current[index] = el;
+                          }}
+                                                  className="relative text-[3rem] font-black cursor-pointer"
                         style={{
                             filter: manualMode
                                 ? isActive
